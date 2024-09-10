e5a1c7361f798e2dd56134130164933c0aaaf0bd
Chris Pravetz <chris@pravetz.net>
2024-09-10 16:20:22 -0400

    Add 3.0 compatibility

--- package.js
+++ package.js
@@ -2,12 +2,12 @@
 Package.describe({
   name: 'socialize:server-presence',
   summary: 'Scalable server presence',
-  version: '1.0.5',
+  version: '1.0.6',
   git: 'https://github.com/copleykj/socialize-server-presence.git',
 });
 
 Package.onUse(function _ (api) {
-  api.versionsFrom(['1.10.2', '2.3']);
+  api.versionsFrom(['1.10.2', '2.3','3.0']);
 
   api.use(['ecmascript', 'mongo'], 'server');
 
